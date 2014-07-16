44b9cefa00c3a0e25225ad8c8cc9dedcd0dda09b
mohamad.salimi <mohamad.salimi@49e7cab3-a93e-4015-927d-bb40bcb649f4>
2014-07-16 13:57:09 +0000

    Getters and Setters added

--- RepoDemo/src/com/fdmgroup/repodemo/User.java
+++ RepoDemo/src/com/fdmgroup/repodemo/User.java
@@ -1,5 +1,20 @@
 package com.fdmgroup.repodemo;
 
 public class User {
+	
+	String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	String password;
 
 }
