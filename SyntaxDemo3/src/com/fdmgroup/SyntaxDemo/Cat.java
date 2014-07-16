1d91eb66bc81c5f7bb0c85edef253a96bc94a278
mohamad.salimi <mohamad.salimi@49e7cab3-a93e-4015-927d-bb40bcb649f4>
2014-07-16 15:08:42 +0000

    Branch for initial import

---
+++ SyntaxDemo3/src/com/fdmgroup/SyntaxDemo/Cat.java
@@ -0,0 +1,34 @@
+package com.fdmgroup.SyntaxDemo;
+
+public class Cat {
+	
+	private boolean fed=false;
+	
+	public boolean isFed(){
+		//More functionality 
+		return fed;
+		
+	}
+	
+	 
+	public boolean isMatted(){
+		return false;
+	}
+	
+	public void feed(){
+		fed=true;
+	}
+	
+	public void stroke(){
+		
+	}
+	
+	public void groom(){
+		
+	}
+	
+	public void newMethod(){
+		
+	}
+
+}
