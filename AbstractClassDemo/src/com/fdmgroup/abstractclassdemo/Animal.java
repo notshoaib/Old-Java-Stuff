f00bb19383327e1735f5346c7f9a34d9e3b8615e
mohamad.salimi <mohamad.salimi@49e7cab3-a93e-4015-927d-bb40bcb649f4>
2014-10-02 15:56:44 +0000

    Initial commit

---
+++ AbstractClassDemo/src/com/fdmgroup/abstractclassdemo/Animal.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.abstractclassdemo;
+
+public abstract class Animal {
+
+	public Animal() {
+		
+	}
+	public abstract void eat();
+	
+	
+	public abstract void sleep(int hours); 
+
+}
---
+++ AbstractClassDemo/src/com/fdmgroup/abstractclassdemo/Cat.java
@@ -0,0 +1,45 @@
+package com.fdmgroup.abstractclassdemo;
+
+public class Cat extends Mammal {
+
+	public Cat() {
+		
+	}
+
+	@Override
+	public void breed() {
+		System.out.println("Cat breeding");
+		
+	}
+
+	@Override
+	public void eat() {
+		System.out.println("Cat is eating");
+		
+	}
+
+	/**This method causes an animal to sleep
+	 * @param hours specifies how long a cat will sleep
+	 */
+	@Override
+	public void sleep(int hours) {
+		System.out.println("Cat is sleeping for "+hours +" "+hours);
+		
+	}
+	
+	@Override
+	public void walk(){
+		System.out.println("Cat is walking like a human");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
